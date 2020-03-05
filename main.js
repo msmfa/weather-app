6799f0c27cec3626daf9476fe1e4e9a14eff0380
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-03-05 19:39:09 +0000

    Updated weather to only show on click

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ const button = document.getElementById("submit-button");
 const temperature = document.querySelector(".temp");
 const userLocation = document.querySelector(".location");
 const description = document.querySelector(".description");
-
+const container = document.getElementById("container");
 button.addEventListener("click", getData);
 
 function getData() {
   let userCity = inputField.value;
   inputField.value = "";
   getWeather(userCity);
+  container.style.display = "block";
 }
 
 async function getWeather(userCity) {
